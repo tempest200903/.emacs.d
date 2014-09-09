8a4ef122aa0705c25f0bbf87b6f4276e3a8c3d8c
NY <ny>
2014-09-09 15:15:57 +0900

    add my-dired-mode-map.el

---
+++ inits/my-dired-mode-map.el
@@ -0,0 +1,6 @@
+;; -*- coding: utf-8-unix; mode: Emacs-Lisp -*-
+;; my-dired-mode-map.el
+;; #+LAST_UPDATED: 2013-11-03
+;; ======================================================================
+;; wdired-change-to-wdired-mode needs my-wdired.el
+(define-key dired-mode-map "r" 'wdired-change-to-wdired-mode)
