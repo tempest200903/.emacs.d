efcb9d988ae521a7345b26193f1179d6fac63e49
tempest200903 <tempest200903@gmail.com>
2014-09-11 00:55:53 +0900

    package-install japanese-holidays

---
+++ vendor/elpa/japanese-holidays-20140620.559/japanese-holidays-autoloads.el
@@ -0,0 +1,18 @@
+;;; japanese-holidays-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("japanese-holidays-pkg.el" "japanese-holidays.el")
+;;;;;;  (21520 29262 758000))
+
+;;;***
+
+(provide 'japanese-holidays-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; japanese-holidays-autoloads.el ends here
